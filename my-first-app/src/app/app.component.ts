9b9cd9daa56c06a14774a13f31edf8d2813e2f48
JJuarez <jose.juarez@yuxipacific.com>
2018-08-06 17:08:16 -0500

    Creando server.component manualmente, sin usar Angular CLI

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mi-componente',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
