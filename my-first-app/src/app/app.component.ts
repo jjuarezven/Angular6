1e72b11fb27fb5cfc248dddd0507c2b1cdfa1c28
JJuarez <jose.juarez@yuxipacific.com>
2018-08-09 17:07:25 -0500

    opciones de componentes, elementos, atributos, clases, css

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mi-componente',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles:[`
+    h3 { color: red; }
+  `]
 })
 export class AppComponent {
   name = 'JOSE';
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // componente como elemento
   selector: 'app-servers',
+  // componente como atributo selector: '[app-servers]',
+  // componente como clase selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
