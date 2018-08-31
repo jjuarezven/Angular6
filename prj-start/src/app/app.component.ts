c1baf65efe617a841b9d1a3f5383c2029da253a6
JJuarez <jose.juarez@yuxipacific.com>
2018-08-31 11:38:36 -0500

    navegando entre componentes usando event binding y ngIf

--- prj-start/src/app/app.component.ts
+++ prj-start/src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedOption = 'recipe';
+  onNavigate(option: string) {
+    this.loadedOption = option;
+  }
 }
