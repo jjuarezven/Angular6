ccbd563b2053db744d98a1789d501f808548b33e
JJuarez <jose.juarez@yuxipacific.com>
2018-10-17 12:41:17 -0500

    boton Add de shopping-list funcional

--- prj-start/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-start/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-
+export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
-  ngOnInit() {
+  addIngredient() {
+    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
