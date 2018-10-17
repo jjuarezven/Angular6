5d422d961d0167bfe204f46ef75843c8790b636b
JJuarez <jose.juarez@yuxipacific.com>
2018-10-17 13:54:02 -0500

    appBasicHighLight funcionando

---
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,13 @@
+import { OnInit, Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighLight]'
+})
+
+export class BasicHighLightDirective implements OnInit {
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+}
