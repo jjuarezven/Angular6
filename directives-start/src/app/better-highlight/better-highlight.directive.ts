f770b473aa80405e9f253563969b9a05b4b2969a
JJuarez <jose.juarez@yuxipacific.com>
2018-10-17 16:21:27 -0500

    usando @HostBinding, nos permite no usar el Renderer

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,23 +1,25 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 import { MockNgModuleResolver } from '@angular/compiler/testing';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit{
+export class BetterHighlightDirective implements OnInit {
+  // otra manera, sin usar el Renderer
+  @HostBinding('style.backgroundColor') backgrounColor: string;
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
-
-  }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     /* this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'lightBlue'); */
   }
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'lightBlue');
+    this.backgrounColor = 'lightBlue';
+    /* this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'lightBlue'); */
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgrounColor = 'transparent';
+    /* this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent'); */
   }
 }
