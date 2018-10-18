62321319689883f4484c6de6dfee84c77a6c49db
JJuarez <jose.juarez@yuxipacific.com>
2018-10-18 15:36:18 -0500

    usando servicios para comunicacion entre componentes: mediante el evento statusUpdated definido en accountService, desde accountComponent se emite el evento y new-account lo recibe

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -13,9 +13,13 @@ import { AccountsService } from '../services/accounts.service';
 export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe((status: string) =>
+      alert('new status: ' + status)
+    );
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
+
   }
 }
