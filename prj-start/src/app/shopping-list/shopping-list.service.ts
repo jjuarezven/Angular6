75c1b5a2d64c76121aa3c8500679c3d3642c555f
JJuarez <jose.juarez@yuxipacific.com>
2018-10-19 17:19:10 -0500

    se pasan los ingredientes de un recipe-detail a shopping-list service, utilizando como intermediario al recipe service

--- prj-start/src/app/shopping-list/shopping-list.service.ts
+++ prj-start/src/app/shopping-list/shopping-list.service.ts
@@ -18,4 +18,10 @@ export class ShoppingListService {
     // emite el evento a todos los suscritos para que actualicen sus datos
     this.ingredientChanged.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // el operador de ES6 spread (...) permite convertir un array [1,2,3] a lista 1,2,3, para que asi push pueda aceptar la lista
+    this.ingredients.push(...ingredients);
+    this.ingredientChanged.emit(this.ingredients.slice());
+  }
 }
