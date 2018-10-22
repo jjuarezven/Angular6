63db033811abb983d247b390068d1c2658ffca7d
JJuarez <jose.juarez@yuxipacific.com>
2018-10-22 11:48:01 -0500

    route.params es un obleto observable, podemos suscribirnos a sus cambios para tener los yltimos paramteros proveidos y cambiar la informacion consecuentemente

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number; name: string };
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute) {}
+/*
+The snapshot is fine to use if there isn't any need to observe any changes to the activate route.
+If the route is going to change and we need to capture that and react to it, then the reactive approach is the correct method.
+*/
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+    this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
   }
-
 }
