0f2b544211d626e3153142b129c36c1fcbe6801b
JJuarez <jose.juarez@yuxipacific.com>
2018-10-22 16:28:35 -0500

    redirecting y wildcards

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,10 +12,15 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [{ path: ':id:name', component: UserComponent }] },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [{ path: ':id:name', component: UserComponent }]
+  },
   {
     path: 'servers',
     component: ServersComponent,
@@ -23,7 +28,9 @@ const appRoutes: Routes = [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent }
     ]
-  }
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -34,7 +41,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
