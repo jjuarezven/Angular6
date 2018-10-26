97eaeadc1c703e65f5cb065f556d4f547c8a3917
JJuarez <jose.juarez@yuxipacific.com>
2018-10-26 17:11:55 -0500

    cleaning y agregando subscribe

--- prj-start/src/app/shopping-list/shopping-list.component.ts
+++ prj-start/src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private shoppingService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingService.getIngredients();
     // cuando se agrega un elemento al array del servicio, se emite una notificacion con los datos correspondientes
-    this.shoppingService.ingredientChanged.subscribe((ingredients: Ingredient[]) => this.ingredients = ingredients);
+    this.subscription = this.shoppingService.ingredientChanged.subscribe((ingredients: Ingredient[]) => this.ingredients = ingredients);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- prj-start/src/app/shopping-list/shopping-list.service.ts
+++ prj-start/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
-  ingredientChanged = new EventEmitter<Ingredient[]>();
+  ingredientChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('pasta', 10),
@@ -16,12 +16,12 @@ export class ShoppingListService {
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     // emite el evento a todos los suscritos para que actualicen sus datos
-    this.ingredientChanged.emit(this.ingredients.slice());
+    this.ingredientChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     // el operador de ES6 spread (...) permite convertir un array [1,2,3] a lista 1,2,3, para que asi push pueda aceptar la lista
     this.ingredients.push(...ingredients);
-    this.ingredientChanged.emit(this.ingredients.slice());
+    this.ingredientChanged.next(this.ingredients.slice());
   }
 }
