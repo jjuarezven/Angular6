a3a728cfd161e54e76f41b21ce60ae09202da3d9
JJuarez <jose.juarez@yuxipacific.com>
2018-11-01 14:42:59 -0500

    template driven form

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // tenemos acceso al formulario
+  @ViewChild('f')
+  signupForm: NgForm;
+  answer = '';
+  defaultQuestion = 'pet';
+  genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  /* onSubmit(form: NgForm) {
+    console.log(form);
+  } */
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.signupForm.reset();
+  }
+}
