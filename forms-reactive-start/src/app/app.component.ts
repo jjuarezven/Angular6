87837aa06b88b4de26de669d57d497b805d27256
JJuarez <jose.juarez@yuxipacific.com>
2019-01-31 16:14:04 -0500

    reactive forms, reaccionando al cambio del estatus o los valores del formulario

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -16,12 +16,22 @@ export class AppComponent implements OnInit {
     this.singupForm = new FormGroup({
       // form anidado
       userData: new FormGroup({
+        /* Explicacion del BIND: If in the validator method the this keyword is used (when pointing to a class property of this component),
+        the value of this has to be bound when the method is called, to make sure that this points to this component instance,
+        as it should. */
         username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([])
     });
+    /* se puede usar tambien en cada control del formulario */
+    /* this.singupForm.valueChanges.subscribe((value) =>
+      console.log(value)
+    ); */
+    this.singupForm.statusChanges.subscribe((status) =>
+      console.log(status)
+    );
   }
 
   onSubmit() {
