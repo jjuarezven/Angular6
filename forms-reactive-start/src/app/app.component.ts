b745b87f34c381435e39bbaab8d39a08edff8dbe
JJuarez <jose.juarez@yuxipacific.com>
2019-01-31 15:23:55 -0500

    reactive forms, agregando validaciones personalizadas

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  genders = ["male", "female"];
-
+  genders = ['male', 'female'];
   singupForm: FormGroup;
+  forbiddenUserNames = ['Chris', 'Ana'];
 
   ngOnInit() {
     this.singupForm = new FormGroup({
       // form anidado
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         email: new FormControl(null, [Validators.required, Validators.email])
       }),
-      gender: new FormControl("male"),
+      gender: new FormControl('male'),
       hobbies: new FormArray([])
     });
   }
@@ -28,7 +28,14 @@ export class AppComponent implements OnInit {
   }
 
   onAddHobby() {
+    // agregaremos un control en tiempo de ejecucion
     const controls = new FormControl(null, Validators.required);
     (<FormArray>this.singupForm.get('hobbies')).push(controls);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
   }
 }
