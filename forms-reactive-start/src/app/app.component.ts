d5016030a9071a8f3357a14c7bb71ec6b8201632
JJuarez <jose.juarez@yuxipacific.com>
2019-01-31 14:25:31 -0500

    reactive forms, despues de agrupar controles

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
+  genders = ["male", "female"];
 
   singupForm: FormGroup;
 
   ngOnInit() {
-    this.singupForm = new FormGroup( {
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      gender: new FormControl('male')
+    this.singupForm = new FormGroup({
+      // form anidado
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email])
+      }),
+      gender: new FormControl("male")
     });
   }
 
