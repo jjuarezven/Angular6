0356970b352c5f12b9c7188a3a04ae32957bc119
JJuarez <jose.juarez@yuxipacific.com>
2019-02-06 10:08:23 -0500

    actualizando datos usando put

--- http-start/src/app/server.service.ts
+++ http-start/src/app/server.service.ts
@@ -9,11 +9,13 @@ export class ServerService {
 
   storeServers(servers: any[]) {
     const headers = new Headers({'Content-Type': 'application/json'});
-    return this.http.post('https://practica-http.firebaseio.com/data.json', servers, {headers: headers});
+    /* return this.http.post('https://practica-http.firebaseio.com/data.json', servers, {headers: headers}); */
+    // actualizar datos
+    return this.http.put('https://practica-http.firebaseio.com/data.json', servers, {headers: headers});
   }
 
   getServers() {
     return this.http.get('https://practica-http.firebaseio.com/data.json');
   }
-  }
+
 }
