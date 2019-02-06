2a3e4f28302486f0cba50db0a089592619af10de
JJuarez <jose.juarez@yuxipacific.com>
2019-02-06 10:44:31 -0500

    utilizando los datos retornados

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -1,27 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ServerService } from "./server.service";
-import { Response } from '@angular/http';
+import { Response } from "@angular/http";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private serverService: ServerService) {}
+  servers = [];
 
-  servers = [
-    {
-      name: "Testserver",
-      capacity: 10,
-      id: this.generateId()
-    },
-    {
-      name: "Liveserver",
-      capacity: 100,
-      id: this.generateId()
+  ngOnInit() {
+    this.onGet();
+    if (this.servers.length === 0) {
+      this.servers = [
+        {
+          name: "Testserver",
+          capacity: 10,
+          id: this.generateId()
+        },
+        {
+          name: "Liveserver",
+          capacity: 100,
+          id: this.generateId()
+        }
+      ];
     }
-  ];
+  }
+
   onAddServer(name: string) {
     this.servers.push({
       name: name,
@@ -42,19 +49,17 @@ export class AppComponent {
       );
   }
 
-  onGet(){
-    this.serverService
-      .getServers()
-      .subscribe(
-        // cambiamos la transformacion al servicio en lugar de hacerla en cada sitio donde se invoque el metodo
-        /* (response : Response) => {
+  onGet() {
+    this.serverService.getServers().subscribe(
+      // cambiamos la transformacion al servicio en lugar de hacerla en cada sitio donde se invoque el metodo
+      /* (response : Response) => {
           const data = response.json();
           console.log(data);
         }, */
-        (servers: any) => {
-          console.log(servers);
-        },
-        error => console.log(error)
-      );
+      (servers: any) => {
+        this.servers = servers;
+      },
+      error => console.log(error)
+    );
   }
 }
--- http-start/src/app/server.service.ts
+++ http-start/src/app/server.service.ts
@@ -4,23 +4,30 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ServerService {
-  constructor(private http: Http) {
-
-  }
+  constructor(private http: Http) {}
 
   storeServers(servers: any[]) {
-    const headers = new Headers({'Content-Type': 'application/json'});
+    const headers = new Headers({ 'Content-Type': 'application/json' });
     /* return this.http.post('https://practica-http.firebaseio.com/data.json', servers, {headers: headers}); */
     // actualizar datos
-    return this.http.put('https://practica-http.firebaseio.com/data.json', servers, {headers: headers});
+    return this.http.put(
+      'https://practica-http.firebaseio.com/data.json',
+      servers,
+      { headers: headers }
+    );
   }
 
   getServers() {
     // cambiamos la transformacion al servicio en lugar de hacerla en cada sitio donde se invoque el metodo
-    return this.http.get('https://practica-http.firebaseio.com/data.json')
-    .map((response: Response) => {
-      return response.json();
-    });
+    return this.http
+      .get('https://practica-http.firebaseio.com/data.json')
+      .map((response: Response) => {
+        const data = response.json();
+        // por medio de map agregamos la propiedad name
+        for (const server of data) {
+          server.name = 'FETCHED_' + server.name;
+        }
+        return data;
+      });
   }
-
 }
