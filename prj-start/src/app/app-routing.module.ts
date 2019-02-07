a6c8c28f07550778ff8a2e2ef6562a7d82f347d7
JJuarez <jose.juarez@yuxipacific.com>
2019-02-07 15:55:30 -0500

    creando modulos para shopping list y auth

--- prj-start/src/app/app-routing.module.ts
+++ prj-start/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 const appRoutes: Routes = [
   // pagina inicial. path vacio '' es parte de todas las rutas asi que se debe usar pathMatch: 'full' para evitar error
@@ -10,10 +8,7 @@ const appRoutes: Routes = [
   {
     path: 'shopping-list',
     component: ShoppingListComponent
-  },
-  // signup
-  { path: 'signup', component: SignupComponent },
-  { path: 'signin', component: SigninComponent }
+  }
 ];
 
 @NgModule({
