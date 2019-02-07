73853b91332456ea5401cb2eccbef4277d6c1725
JJuarez <jose.juarez@yuxipacific.com>
2019-02-07 14:37:27 -0500

    definiendo y usando un feature module (recipes)

--- prj-start/src/app/app-routing.module.ts
+++ prj-start/src/app/app-routing.module.ts
@@ -1,32 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
   // pagina inicial. path vacio '' es parte de todas las rutas asi que se debe usar pathMatch: 'full' para evitar error
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    children: [
-      // por defecto, ningun recipe seleccionado
-      { path: '', component: RecipeStartComponent },
-      // el mismo componente tanto para creacion como para edicion.
-      // Every time the static path should come before the dynamic one para evitar errores
-      { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
-      // recipe detail
-      { path: ':id', component: RecipeDetailComponent },
-      { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
-    ]
-  },
   {
     path: 'shopping-list',
     component: ShoppingListComponent
---
+++ prj-start/src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,36 @@
+
+// tenemos que crear un routingModule por cada modulo que queramos agregar
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { AuthGuard } from "../auth/auth-guard.service";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+
+const recipesRoutes: Routes = [
+  {
+    path: "recipes",
+    component: RecipesComponent,
+    children: [
+      // por defecto, ningun recipe seleccionado
+      { path: "", component: RecipeStartComponent },
+      // el mismo componente tanto para creacion como para edicion.
+      // Every time the static path should come before the dynamic one para evitar errores
+      { path: "new", component: RecipeEditComponent, canActivate: [AuthGuard] },
+      // recipe detail
+      { path: ":id", component: RecipeDetailComponent },
+      {
+        path: ":id/edit",
+        component: RecipeEditComponent,
+        canActivate: [AuthGuard]
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(recipesRoutes)],
+  exports: [RouterModule]
+})
+export class RecipesRoutingModule {}
