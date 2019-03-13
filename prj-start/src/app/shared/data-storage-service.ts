7855316d8981e6e35e01258113fc36aa36b6d44e
JJuarez <jose.juarez@yuxipacific.com>
2019-03-13 14:11:41 -0500

    x

--- prj-start/src/app/shared/data-storage-service.ts
+++ prj-start/src/app/shared/data-storage-service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from "@angular/core";
+import { Response } from "@angular/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 import "rxjs/add/operator/map";
-<<<<<<< HEAD
-=======
 import { AuthService } from "../auth/auth.service";
->>>>>>> 1376f97e3ae27be6339a8f98b006b5107f971b5f
 import { HttpClient, HttpRequest, HttpParams } from "@angular/common/http";
 
 @Injectable()
 export class DataStorageService {
   constructor(
     private httpClient: HttpClient,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private authService: AuthService
   ) {}
 
   storeRecipes() {
-<<<<<<< HEAD
-=======
     const token = this.authService.getToken();
 
->>>>>>> 1376f97e3ae27be6339a8f98b006b5107f971b5f
     /* return this.httpClient.put(
       "https://ng-recipe-book-f0b42.firebaseio.com/recipes.json",
       this.recipeService.getRecipes(),
@@ -37,26 +33,22 @@ export class DataStorageService {
       {
         reportProgress: true,
         // headers: new HttpHeaders().set('Authorization', 'Bearer sssss') // se pueden configurar headers
-<<<<<<< HEAD
-        // ahora el token va a ser agregado por el interceptor
-        //params: new HttpParams().set("auth", token)
-=======
         params: new HttpParams().set("auth", token)
->>>>>>> 1376f97e3ae27be6339a8f98b006b5107f971b5f
       }
     );
     return this.httpClient.request(req);
   }
 
   getRecipes() {
+    const token = this.authService.getToken();
+
     return (
       this.httpClient
         .get<Recipe[]>(
           "https://ng-recipe-book-f0b42.firebaseio.com/recipes.json",
-          // ahora el token va a ser agregado por el interceptor
-          /* {
+          {
             params: new HttpParams().set("auth", token)
-          } */
+          }
         )
         // infiere el tipo de respuesta
         /* .map((response: Response) => {
